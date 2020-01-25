d9c6d8780f59ba44e9f69901990535bb881b8dea
horiuchi-mitsushi <m.horiuchi@pricelesslab.co.jp>
2020-01-25 16:54:41 +0900

    no message

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ function calc(mode) {
     let anser = document.getElementById('anser');
     let ret;
     
-    document.getElementById("cFrame").src = "https://roote.ekispert.net/result?arr=%E5%A4%A7%E9%98%AA&arr_code=25853&connect=true&dep=%E6%9D%B1%E4%BA%AC&dep_code=22828&highway=true&hour&limitedExpress=true&liner=true&local=true&minute&plane=true&shinkansen=true&ship=true&sleep=false&sort=time&surcharge=3&type=dep&via1=&via1_code=&via2=&via2_code=";
+    document.getElementById("cFrame").src = "";
 }
