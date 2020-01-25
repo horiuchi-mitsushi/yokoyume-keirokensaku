516f85ec1675fa0041f5376882167e872990c709
horiuchi-mitsushi <m.horiuchi@pricelesslab.co.jp>
2020-01-25 16:44:03 +0900

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+/* 簡易計算機 */
+function calc(mode) {
+    let valueA = document.getElementById('value_a').value;
+    let valueB = document.getElementById('value_b').value;
+    let anser = document.getElementById('anser');
+    let ret;
+    
+    document.getElementById("cFrame").src = "https://roote.ekispert.net/result?arr=%E5%A4%A7%E9%98%AA&arr_code=25853&connect=true&dep=%E6%9D%B1%E4%BA%AC&dep_code=22828&highway=true&hour&limitedExpress=true&liner=true&local=true&minute&plane=true&shinkansen=true&ship=true&sleep=false&sort=time&surcharge=3&type=dep&via1=&via1_code=&via2=&via2_code=";
+}
