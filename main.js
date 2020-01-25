5792ee60b2ef4b1d5e5f050c66d8540713e9058e
horiuchi-mitsushi <m.horiuchi@pricelesslab.co.jp>
2020-01-25 16:56:40 +0900

    no message

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ function calc(mode) {
     let anser = document.getElementById('anser');
     let ret;
     
-    document.getElementById("cFrame").src = "";
+    document.getElementById("cFrame").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3256.9865612533645!2d139.6683934172677!3d35.28146037992259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018400a8453ffff%3A0x5eeffc0cee828ffc!2s16%20Startups!5e0!3m2!1sja!2sjp!4v1571388450688!5m2!1sja!2sjp";
 }
